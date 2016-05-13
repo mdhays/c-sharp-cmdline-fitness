2ad45fdc2f7d197553bbabb2789a0221626a2e50
Matthew <mdhays1995@gmail.com>
2016-05-13 13:28:17 -0500

    Added some text to be displayed on running the app.

--- program.cs
+++ program.cs
@@ -2,6 +2,18 @@ class Program
 {
     static void Main()
     {
+        // Prompt the user for minutes exercised.
+        Console.Write("Enter how many minutes you exercised: ");
+        
+        string entry = Console.ReadLine();
+        
+        // Add minutes exercised to total
+        // Display total minutes exercised on screen
+        Console.Write("You've entered");
+        Console.Write(entry);
+        Console.WriteLine(" minutes");
+        
+        //Repeat until user quits.
         
     }
 }
