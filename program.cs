27c8ad474ecb961ac72dd35971d727f0c9ba3bd3
Matthew <mdhays1995@gmail.com>
2016-05-22 19:53:09 -0500

    Functionality is fixed, now tracking minutes exercised properly.

--- program.cs
+++ program.cs
@@ -5,41 +5,40 @@ namespace cSharp.fitness
 
     class Program
     {
+
         static void Main()
         {
-            int runningTotal = 0;
-            bool keepGoing = true;
-            
-            while (keepGoing)
-            {             
-            
-                // Prompt the user for minutes exercised.
-                 Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
-                string entry = Console.ReadLine();
-                
 
-                
+            int runningTotal = 0;
+
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+
+                string entry = Console.ReadLine();
+
                 if (entry == "quit")
                 {
                     keepGoing = false;
                 }
                 else
-                {                   
-
+                {
                     int minutes = int.Parse(entry);
-                
-                     // Add minutes exercised to total
+
                     runningTotal = runningTotal + minutes;
 
-        
-                    // Display total minutes exercised on screen        
-                    Console.WriteLine("You've exercised " + entry + " minutes");
+                    Console.WriteLine("You've exercised " + runningTotal + " minutes");
                 }
-                    // Repeat until user quits.
+
             }
-                     Console.WriteLine("Goodbye");
-                
-            
+
+            Console.WriteLine("Goodbye");
+
         }
+
     }
+
 }
